ea9513f4b36c128dc4b1ec9a7c45ef49eaa1e302
Jordy Webb <webb.jordy@gmail.com>
2017-03-26 04:56:22 -0700

    refactor routes

--- client/main.js
+++ client/main.js
@@ -1,47 +1,18 @@
 import { Meteor } from 'meteor/meteor'
-import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
 import { Tracker } from 'meteor/tracker';
+
+import { routes, onAuthChange } from '../imports/routes/routes';
 
 import Signup from '../imports/ui/signup';
 import Link from '../imports/ui/link';
 import NotFound from '../imports/ui/notfound';
 import Login from '../imports/ui/login';
 
-const unauthenticatedPages = ['/', 'signup'];
-const authenticatedPages = ['/links'];
-const onEnterPublicPage = () => {
-  if(Meteor.userId()) {
-    browserHistory.replace('/links');
-  }
-};
-const onEnterPrivatePage = () => {
-  if(!Meteor.userId()) {
-    browserHistory.replace('/');
-  }
-};
-const routes = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Login} onEnter={onEnterPublicPage} />
-    <Route path="/signup" component={Signup} onEnter={onEnterPublicPage} />
-    <Route path="/links" component={Link} onEnter={onEnterPrivatePage} />
-    <Route path="*" component={NotFound} />
-  </Router>
-);
 
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
-  const pathname = browserHistory.getCurrentLocation().pathname;
-  const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
-  const isAuthenticatedPage = authenticatedPages.includes(pathname);
-  console.log('isAuthenticated', isAuthenticated);
-
-  if (isUnauthenticatedPage && isAuthenticated) {
-    browserHistory.replace('/links');
-  } else if (isAuthenticatedPage && !isAuthenticated) {
-    browserHistory.replace('/');
-  }
+  onAuthChange(isAuthenticated);
 });
 
 Meteor.startup(() => {
--- server/main.js
+++ server/main.js
@@ -1,40 +1,8 @@
 import { Meteor } from 'meteor/meteor';
-import SimpleSchema from 'simpl-schema';
-import { Accounts } from 'meteor/accounts-base';
-//
-// const empSchema = new SimpleSchema({
-//   name: {
-//     type: String,
-//     min: 1,
-//     max: 200,
-//   },
-//   hourlyWage: {
-//     type: Number,
-//     min: 1
-//   },
-//   email: {
-//     type: String,
-//     regEx: SimpleSchema.RegEx.Email
-//   }
-// });
+
+import '../imports/api/users';
 
 Meteor.startup(() => {
   // code to run on server at startup
-  Accounts.validateNewUser((user) => {
-    const email = user.emails[0].address;
 
-    try {
-      new SimpleSchema({
-        email: {
-          type: String,
-          regEx: SimpleSchema.RegEx.Email
-        }
-      }).validate({email})
-    } catch (e) {
-      throw new Meteor.Error(400, e.message);
-    }
-
-    console.log('this is the user', user);
-    return true;
-  })
 });
