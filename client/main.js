c8f578ad031aa7201441b94976502b94135d44f4
Jordy Webb <webb.jordy@gmail.com>
2017-03-26 06:21:50 -0700

    add LinksList component

--- client/main.js
+++ client/main.js
@@ -4,16 +4,12 @@ import { Tracker } from 'meteor/tracker';
 
 import { routes, onAuthChange } from '../imports/routes/routes';
 
-import Signup from '../imports/ui/signup';
-import Link from '../imports/ui/link';
-import NotFound from '../imports/ui/notfound';
-import Login from '../imports/ui/login';
-
-
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
   onAuthChange(isAuthenticated);
 });
+
+
 
 Meteor.startup(() => {
   ReactDOM.render(routes, document.getElementById('app'));
