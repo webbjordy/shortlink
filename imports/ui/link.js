b1065366741e5a68a101b1bf691ea568a47787ff
Jordy Webb <webb.jordy@gmail.com>
2017-03-27 09:11:40 -0700

    refactor privatelink and addlink components

--- imports/ui/link.js
+++ imports/ui/link.js
@@ -1,36 +1,17 @@
 import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { Accounts } from 'meteor/accounts-base';
 
 import { Links } from '../api/links';
 import LinksList from './LinksList';
+import PrivateHeader from './privateheader';
+import AddLink from './addlink'
 
 export default class Link extends React.Component {
-  onLogout() {
-    Accounts.logout();
-  }
-  onSubmit(e) {
-    const url = this.refs.url.value.trim();
-
-    e.preventDefault();
-
-    if (url) {
-      Meteor.call('links.insert', url);
-      //Links.insert({ url, userId: Meteor.userId() });
-      this.refs.url.value = '';
-    }
-  }
   render() {
     return (
       <div>
-        <h1>Your Links</h1>
-        <button onClick={this.onLogout.bind(this)}>Logout</button>
+        <PrivateHeader title="your links" />
         <LinksList/>
-        <p>Add Link</p>
-        <form onSubmit={this.onSubmit.bind(this)}>
-            <input type="text" ref="url" placeholder="URL"/>
-            <button>Add Link</button>
-        </form>
+        <AddLink />
       </div>
     );
   }
