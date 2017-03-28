34b301b067ffe7d3822e84216c5dae97002cddac
Jordy Webb <webb.jordy@gmail.com>
2017-03-27 18:27:19 -0700

    install clipboard

--- imports/ui/linkslistitem.js
+++ imports/ui/linkslistitem.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Meteor } from 'meteor/meteor';
+import Clipboard from 'clipboard';
 
 export default class LinksListItem extends React.Component {
+  componentDidMount() {
+    this.clipboard = new Clipboard(this.refs.copy);
+
+    this.clipboard.on('success', () => {
+      alert('YEAH')
+    }).on('error', () =>{
+      alert('Fucked up')
+    })
+  }
+componentWillUnmount() {
+  this.clipboard.destroy();
+}
   render() {
     return (
       <div>
         <p>{this.props.url}</p>
         <p>{this.props.shortUrl}</p>
+        <button ref="copy" data-clipboard-text={this.props.shortUrl}>Copy</button>
       </div>
 
     );
